9cd8c91e2449f2856b5bd3347cbd47a5dcb6e6f4
rohan2RS <guptarohan30112005@gmail.com>
2026-08-01 18:46:38 +0530

    Add Spring Boot application

---
+++ app/src/main/java/com/hello/app/HelloController.java
@@ -0,0 +1,12 @@
+package com.hello.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){return "Hello from CI/CD pipeline";}
+
+
+}
